ecf2c4e02737afbd6bbbc46d6be1db170042ca0d
Yang He <yang.he@126.com>
2026-08-21 01:31:09 +0800

    build(deps): replace discontinued flutter_markdown with flutter_markdown_plus
    OPT-016: flutter_markdown 0.7.7+1 is discontinued on pub.dev. Switch to
    flutter_markdown_plus 1.0.12, the maintained community fork with the same
    MarkdownBody API. Update dialog rendering is unchanged; analyze and all
    78 tests still pass.

--- lib/services/update_service.dart
+++ lib/services/update_service.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:onlystudy/l10n/app_localizations.dart';
